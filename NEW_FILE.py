61f80a6dbc36caef438f9a9c1890ae9e25e08efc
xxTanja <116067536+xxTanja@users.noreply.github.com>
2022-11-03 11:12:17 +0100

    hello

---
+++ NEW_FILE.py
@@ -0,0 +1,69 @@
+a =3425435
+b=32432.34
+print(a)
+print(type(a))
+
+print(b)
+print(type(b))
+#c= a+int(b)
+c=   round(b)
+print(c)
+print(type(c))
+
+print('------------')
+d=343673143189563866126589
+print(d)
+
+e=d+1
+print(e)
+
+f=343673143189563866126589.6
+print(f)
+
+g =f+1001000
+print(g)
+
+h=0.000000000000000000000000000000000003343
+print (h)
+
+i =1.79e308
+print(i)
+
+i=1.8e308
+print(i)
+
+print('-------')
+#bb = Ob101
+#print(bb)
+
+#hx =Ox1a
+#print(hx)
+
+print('-------')
+
+j=2000
+k=50
+print(type(j/k))
+
+k=5
+h=k**3
+print(h)
+g =12
+print(g%8)
+
+s1='653'
+s2='63.75894'
+
+print(s1.isnumeric())
+print(s2.isnumeric())
+
+a=int(s1)
+b=float(s2)
+print(a+b)
+
+
+print(round(b,2))
+
+#copy numeric file
+
+#new comment
